59d42866dd7a3bffafba4c3f33117f268a5386fd
Andrés Alberto Torres García <andresalbertotorresgarcia@gmail.com>
2024-08-20 18:44:07 -0500

    feat(address): added address value object

---
+++ Shared/Domain/Model/ValueObjects/Address.cs
@@ -0,0 +1,6 @@
+﻿namespace OPPSample.Shared.Domain.Model.ValueObjects;
+
+public record Address(string Street, string Number, string City, string State, string ZipCode, string Country)
+{
+    public string AddressAsString => $"{Street}, {Number}, {City}, {State}, {ZipCode}, {Country}";
+}
