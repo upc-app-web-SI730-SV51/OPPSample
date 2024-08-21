8cc806132f660d7c2862ce6ccfa2a1e578006412
Andrés Alberto Torres García <andresalbertotorresgarcia@gmail.com>
2024-08-20 19:17:02 -0500

    feat(sales-order): added sales order item entity

---
+++ Sales/Domain/Model/Aggregates/SalesOrderItem.cs
@@ -0,0 +1,51 @@
+﻿namespace OPPSample.Sales.Domain.Model.Aggregates;
+
+public class SalesOrderItem
+{
+    public SalesOrderItem(Guid salesOrderId, int productId, int quantity, double unitPrice)
+    {
+        if (salesOrderId == Guid.Empty)
+        {
+            throw new ArgumentException("Sales Order ID is required");
+        }
+
+        if (productId <= 0)
+        {
+            throw new ArgumentException("Product ID is required and must be greater than zeto");
+        }
+
+        if (quantity <= 0)
+        {
+            throw new ArgumentException("Quantity must greater than zero");
+        }
+
+        if (unitPrice <= 0)
+        {
+            throw new ArgumentException("Unit Price must be greater than zero");
+        }
+        
+        SalesOrderId = salesOrderId;
+        ProductId = productId;
+        Quantity = quantity;
+        UnitPrice = unitPrice;
+    }
+    // public section
+
+    public Guid Id { get; private set; } = GenerateOrdenItemId();
+    
+    public Guid SalesOrderId { get; }
+
+    public int ProductId { get; }
+
+    public int Quantity { get;}
+
+    public double UnitPrice { get; }
+
+    public double CalculateItemPrice() => Quantity * UnitPrice;
+    
+    // private section
+    private static Guid GenerateOrdenItemId()
+    {
+        return Guid.NewGuid();
+    }
+}
